81547f3673a6fa455b552c14728a5de1417eb733
Ashish Rao <ashishrao2598@gmail.com>
2019-08-08 13:06:16 +0530

    add main package

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gorilla/handlers"
+	"musicstore/album"
+)
+
+func main() {
+	router := album.NewRouter() // create routes
+
+	// we need to allow access to our api from the front end
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+	// launch server with cors validations
+	log.Fatal(http.ListenAndServe(":8000",
+	handlers.CORS(allowedOrigins, allowedMethods)(router)
+	))
+
+}
